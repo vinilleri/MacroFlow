5b86dbfb08c74aafe9ed14b0a7645e04c237cde0
ViniBiniCini <viniciuscoelhodelima45@gmail.com>
2026-04-02 20:56:06 -0300

    teste

--- src/main/java/com/tcc/macroflow/model/AtividadeFisica.java
+++ src/main/java/com/tcc/macroflow/model/AtividadeFisica.java
@@ -1,4 +1,15 @@
 package com.tcc.macroflow.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AtividadeFisica {
 }
